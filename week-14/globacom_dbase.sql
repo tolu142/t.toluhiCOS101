e42cfd2b005a53b026aac65f77089b775dcb126e
t.toluhiCOS101 <ttoluhi11@gmail.com>
2024-01-26 19:11:56 +0100

    (week-14)

---
+++ week-14/globacom_dbase.sql
@@ -0,0 +1,141 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.1
+-- Dumped by pg_dump version 16.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: department; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.department (
+    dept_managerid integer NOT NULL,
+    dno integer NOT NULL,
+    dname text NOT NULL,
+    dlocation text NOT NULL,
+    pno integer
+);
+
+
+ALTER TABLE public.department OWNER TO postgres;
+
+--
+-- Name: project; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.project (
+    pno integer NOT NULL,
+    pname text NOT NULL,
+    pduration character varying(40) NOT NULL,
+    project_managerid integer
+);
+
+
+ALTER TABLE public.project OWNER TO postgres;
+
+--
+-- Name: staff; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.staff (
+    staff_id integer NOT NULL,
+    staff_name text NOT NULL,
+    dno integer NOT NULL,
+    staff_sal real NOT NULL,
+    age integer NOT NULL,
+    mobile character varying(11) NOT NULL
+);
+
+
+ALTER TABLE public.staff OWNER TO postgres;
+
+--
+-- Data for Name: department; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.department (dept_managerid, dno, dname, dlocation, pno) FROM stdin;
+108	1	Administration	Ikeja	44
+101	2	Account	Egbeda	11
+100	3	Packaging	Ajah	44
+120	4	Research	V.I	33
+97	5	Account	Magodo	22
+122	6	Operations	Mile 2	44
+107	7	Packaging	Ketu	55
+\.
+
+
+--
+-- Data for Name: project; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.project (pno, pname, pduration, project_managerid) FROM stdin;
+11	A	9 Months	102
+22	B	14 Months	97
+33	C	16 Months	120
+44	D	25 Months	108
+55	E	9 Months	107
+\.
+
+
+--
+-- Data for Name: staff; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.staff (staff_id, staff_name, dno, staff_sal, age, mobile) FROM stdin;
+100	Mustapha Ali	3	175000	32	8063285831
+107	Alokwe Martin	7	380000	48	7090082812
+97	Dankade Aminat	5	550000	40	9023688832
+108	Josiah Joshua	1	120000	30	8053189131
+102	Mankinde Mary	2	450000	55	9023487830
+120	Adeleke Jane	4	200000	38	7061045862
+122	Osahon Mark	6	320000	44	8022289842
+117	Suleman Ajayi	3	800000	50	7030089811
+104	Kuti Lawal	1	750000	35	9145689842
+101	Alade Joy	2	250000	33	8023089321
+\.
+
+
+--
+-- Name: department department_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.department
+    ADD CONSTRAINT department_pkey PRIMARY KEY (dno);
+
+
+--
+-- Name: staff employees_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.staff
+    ADD CONSTRAINT employees_pkey PRIMARY KEY (staff_id);
+
+
+--
+-- Name: project project_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.project
+    ADD CONSTRAINT project_pkey PRIMARY KEY (pno);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
