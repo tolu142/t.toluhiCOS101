1e80f3d4d9d4b8170055a14c013b2b37cdf5f1bd
t.toluhiCOS101 <ttoluhi11@gmail.com>
2024-01-26 18:59:39 +0100

    (week-10)

---
+++ week-10/practice_1/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\funsh\OneDrive\Documents\t.toluhiCOS101\week-10\practice_1\target\debug\deps\practice_1.exe: src\main.rs
+
+C:\Users\funsh\OneDrive\Documents\t.toluhiCOS101\week-10\practice_1\target\debug\deps\practice_1.d: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice_2/target/debug/deps/practice_2.d
@@ -0,0 +1,5 @@
+C:\Users\funsh\OneDrive\Documents\t.toluhiCOS101\week-10\practice_2\target\debug\deps\practice_2.exe: src\main.rs
+
+C:\Users\funsh\OneDrive\Documents\t.toluhiCOS101\week-10\practice_2\target\debug\deps\practice_2.d: src\main.rs
+
+src\main.rs:
